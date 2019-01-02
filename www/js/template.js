8dd7e4a100e3a9fc2f4ef4f345dd4091d0df0ada
Louis Larivière <louis.lariviere@gmail.com>
2019-01-02 00:36:37 -0500

    Add files via upload

---
+++ www/js/template.js
@@ -0,0 +1,118 @@
+/*
+* All the template used by bizswap.js
+*/
+
+var base_tpl = '<li class="list-item ii_1">\
+      <div class="item-content">\
+        <div class="item-inner"> \
+          <div class="item-title label" data-i="33" data-id="1">Email<span><i class="fa fa-lock"></i></span></div>\
+          <div class="item-input">\
+            <input type="email" name="33" value="" placeholder="your email" class="base lock" data-label="Email" readonly="true" />\
+          </div>\
+        </div>\
+      </div>\
+    <li class="list-item ii_2">\
+      <div class="item-content">\
+        <div class="item-inner"> \
+          <div class="item-title label" data-i="35" data-id="2">Firstname<span><i class="fa fa-{{lock}}"></i></span></div>\
+          <div class="item-input">\
+            <input type="text" name="35" value="" placeholder="your firstname" class="base {{class}}" data-label="Firstname" readonly="true" />\
+          </div>\
+        </div>\
+      </div>\
+    </li>\
+    <li class="list-item ii_3">\
+      <div class="item-content">\
+        <div class="item-inner"> \
+          <div class="item-title label" data-i="38" data-id="3">Lastname<span><i class="fa fa-{{lock}}"></i></span></div>\
+          <div class="item-input">\
+            <input type="text" name="38" value="" placeholder="your lastname" class="base {{class}}" data-label="Lastname" readonly="true" />\
+          </div>\
+        </div>\
+      </div>\
+    </li>\
+    <li class="list-item ii_4">\
+      <div class="item-content">\
+        <div class="item-inner"> \
+          <div class="item-title label" data-i="26" data-id="4">Cellphone<span><i class="fa fa-{{lock}}"></i></span></div>\
+          <div class="item-input">\
+            <input type="tel" name="26" value="" placeholder="your cellphone" class="base {{class}}" data-label="Cellphone" readonly="true" />\
+          </div>\
+        </div>\
+      </div>\
+    </li>\
+    <li class="list-item ii_5">\
+      <div class="item-content">\
+        <div class="item-inner"> \
+          <div class="item-title label" data-i="29" data-id="5">Company<span><i class="fa fa-{{lock}}"></i></span></div>\
+          <div class="item-input">\
+            <input type="text" name="29" value="" placeholder="your company name" class="base {{class}}" data-label="Company" readonly="true" />\
+          </div>\
+        </div>\
+      </div>\
+    </li>\
+    <li class="list-item ii_6">\
+      <div class="item-content">\
+        <div class="item-inner"> \
+          <div class="item-title label" data-i="40" data-id="6">Job title<span><i class="fa fa-{{lock}}"></i></span></div>\
+          <div class="item-input">\
+            <input type="text" name="40" value="" placeholder="your job title" class="base {{class}}" data-label="Title" readonly="true" />\
+          </div>\
+        </div>\
+      </div>\
+    </li>';
+
+var cards_templates = [
+	'<div style="top:4px;left:0px;font-weight:bold;font-size:16px;">{{complete name}}{{firstname}} {{lastname}}</div>\
+	<div style="top:28px;left:0px;">{{title}}</div>\
+	<div style="top:46px;left:0px;">{{company name}}{{company}}</div>\
+	<div style="top:75px;left:120px;">{{address}}</div>\
+	<div style="top:90px;left:120px;">{{city}} {{state_prov}} {{country}} {{postal code}}</div>\
+	<div style="top:105px;left:120px;">{{website}}</div>\
+	<div style="top:120px;left:120px;">{{email}}</div>\
+	<div style="top:135px;left:120px;">{{cellphone}}</div>\
+	<div style="top:150px;left:120px;">{{fax}}</div>\
+	<div class="img" style="top:75px;left:0px;background-image:url({{logo}});">\
+		<i class="fa fa-user fa-4x" style="margin-top:20px;"></i>\
+	</div>',
+	
+	'<div style="top:4px;left:120px;font-weight:bold;font-size:16px;">{{complete name}}{{firstname}} {{lastname}}</div>\
+	<div style="top:28px;left:120px;">{{title}}</div>\
+	<div style="top:46px;left:120px;">{{company name}}{{company}}</div>\
+	<div style="top:75px;left:120px;">{{address}}</div>\
+	<div style="top:90px;left:120px;">{{city}} {{state_prov}} {{country}} {{postal code}}</div>\
+	<div style="top:105px;left:120px;">{{website}}</div>\
+	<div style="top:120px;left:120px;">{{email}}</div>\
+	<div style="top:135px;left:120px;">{{cellphone}}</div>\
+	<div style="top:150px;left:120px;">{{fax}}</div>\
+	<div class="img" style="top:0px;left:0px;background-image:url({{logo}});">\
+		<i class="fa fa-user fa-4x" style="margin-top:20px;"></i>\
+	</div>',
+	
+	'<div style="top:84px;width:50%;text-align:right;font-weight:bold;font-size:16px;">{{complete name}}{{firstname}} {{lastname}} |</div>\
+	<div style="top:88px;left:50%;width:50%;text-align:left;">&nbsp;{{title}}</div>\
+	<div style="top:104px;width:100%;text-align:center;">{{company name}}{{company}}</div>\
+	<div style="top:120px;width:100%;text-align:center;">{{address}} {{city}}</div>\
+	<div style="top:136px;width:100%;text-align:center;">{{state_prov}} {{country}} {{postal code}}</div>\
+	<div style="top:152px;width:50%;text-align:right;">{{website}} |</div>\
+	<div style="top:152px;left:50%;width:50%;text-align:left;">&nbsp;{{email}}</div>\
+	<div style="top:168px;width:50%;text-align:right;">{{cellphone}} |</div>\
+	<div style="top:168px;left:50%;width:50%;text-align:left;">&nbsp;{{fax}}</div>\
+	<div class="img" style="top:0px;width:75px;height:75px;left:50%;margin-left:-38px;background-image:url({{logo}});">\
+		<i class="fa fa-user fa-4x" style="margin-top:10px;"></i>\
+	</div>'
+];
+
+var input_tpl = '<input type="text" name="{{name}}" value="{{value}}" class="{{class}}" data-label="{{label}}" placeholder="{{placeholder}}" readonly="{{readonly}}" />';
+
+var li_tpl = '<li class="list-item {{data-id}}"> \
+		<div class="item-content"> \
+			<div class="item-inner"> \
+				<div class="item-title label" data-i="{{data-i}}" data-id="{{data-id}}">{{label}}<span><i class="fa fa-{{lock}}"></i></span></div> \
+				<div class="item-input"> \
+					{{input}} \
+				</div> \
+			</div> \
+		</div> \
+	</li>';
+	
