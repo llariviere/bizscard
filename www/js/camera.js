5f16d9ea41154254feeb07cf21a27b2a0383005e
Louis Larivière <louis.lariviere@gmail.com>
2019-06-02 12:17:27 -0400

    Update camera.js

--- www/js/camera.js
+++ www/js/camera.js
@@ -2,171 +2,347 @@
 * camera, image and ocr functions...
 */
 
-var scanImg = { 'recto':{}, 'verso':{} };
+	B.fs_ = null;
+	B.cwd_ = null;
+	
+	$$(".button.card-side").on("click", function(){
+		$$(".button.card-side").removeClass("selected");
+		$$(this).addClass("selected");
+		B.card_side = ($$(this).hasClass("front") ? 'front' : 'back');
+		$$("span.card-side").text(B.card_side);
+		$$("#card-photo > img").addClass("hidden");
+		$$("#card-photo > img."+B.card_side).removeClass("hidden");
+	});
 
-function camera_options(srcType) {
-    var options = {
-        // Some common settings are 20, 50, and 100
-        quality: 50,
-        destinationType: Camera.DestinationType.FILE_URI,
-        // In this app, dynamically set the picture source, Camera or photo gallery
-        sourceType: srcType,
-        encodingType: Camera.EncodingType.JPEG,
-        mediaType: Camera.MediaType.PICTURE,
-        allowEdit: false,
-        correctOrientation: true  //Corrects Android orientation quirks
-    }
-    return options;
-}
-
-function camera_open(selection) {
-	console.log('camera_open()');
-	if (typeof Camera === "undefined") {
-		card_ocr_process({vertices:[{x:0,y:0},{x:0,y:0},{x:0,y:0},{x:0,y:0}],description:''});$$("#card-entry img."+B.card_side).attr("src","img/paper.jpg");return false;
-		if (B.card_side=='other') { 	 	
-			B.croper.destroy();
-			var options = { 
-				url:"img/bcard.jpg", 
-				enableOrientation:true,
-     			boundary: B.crop_opts.boundary 
-     		};
-			B.croper = new Croppie(document.getElementById('crop-box'), options);
-			return false;
-		} else {
-			myApp.alert('Camera not availlable');
-			card_ocr_process((B.card_side == 'verso' ? card_back_data : card_data));
-			$$("#card-entry img."+B.card_side).attr("src","img/bcard.jpg");
+	$$("#capturePhoto").on("click", capturePhoto);
+	$$("#listPhoto").on("click", listPhoto);
+	$$("#savePhoto").on("click", savePhoto);
+	$$("#processPhoto").on("click", processPhoto);
+	
+	function setOptions(srcType) {
+	    var options = {
+	        quality: 50,
+	        targetHeight: 1024,
+	        targetWidth: 1024,
+	        destinationType: Camera.DestinationType.FILE_URI,
+	        sourceType: srcType,
+	        encodingType: Camera.EncodingType.PNG,
+	        mediaType: Camera.MediaType.PICTURE,
+	        allowEdit: false,
+	        correctOrientation: true  //Corrects Android orientation quirks
+	    }
+	    return options;
+	}
+	
+	function savePhoto() {
+		
+		if (B.fromfile) {
+			$$('#card-photo-front').attr("src","");
+			$$('#card-photo-back').attr("src","");
+			$$(".button.card-side.back").trigger("click");
+			$$(".button.card-side.front").trigger("click");
 			return false;
 		}
+		var ImageUri = { 
+			front:$$('#card-photo-front').attr("src"),
+			back:$$('#card-photo-back').attr("src")
+		}
+		var now = Date.now();
+		B.dirname = now.toString();
+
+		if (ImageUri.front) {
+			console.log("1 : "+ImageUri.front);
+			window.resolveLocalFileSystemURL(ImageUri.front, function (fileEntry) {
+				console.log("2");
+				window.requestFileSystem(LocalFileSystem.TEMPORARY, 0, function(fileSys) {
+					B.fs_ = fileSys;
+					B.cwd_ = B.fs_.root;
+					console.log("3");
+					B.cwd_.getDirectory(B.dirname, {create:true, exclusive: false}, function(dirEntry) {
+						B.cwd_ = dirEntry;
+						fileEntry.moveTo(dirEntry, "front.png", function(){
+							console.log("front.png moved!");
+							$$('#card-photo-front').attr("src","");
+					   	$$("#savePhoto, #processPhoto").parent().addClass("hidden");
+					   	$$("#listPhoto").parent().removeClass("hidden");
+						}, onFail0);
+					}, onFail1);
+				}, onFail2);
+			}, onFail3);
+			
+		}
+		
+		if (ImageUri.back) {
+			console.log("1 : "+ImageUri.back);
+			window.resolveLocalFileSystemURL(ImageUri.back, function (fileEntry) {
+				window.requestFileSystem(LocalFileSystem.TEMPORARY, 0, function(fileSys) {
+					B.fs_ = fileSys;
+					B.cwd_ = B.fs_.root;
+					B.cwd_.getDirectory(B.dirname, {create:true, exclusive: false}, function(dirEntry) {
+						B.cwd_ = dirEntry;
+						fileEntry.moveTo(dirEntry, "back.png", function(){
+							console.log("back.png moved!");
+							$$('#card-photo-back').attr("src","");
+					   	$$("#savePhoto, #processPhoto").parent().addClass("hidden");
+					   	$$("#listPhoto").parent().removeClass("hidden");
+						}, onFail0);
+		         }, onFail1);
+				}, onFail2);
+			}, onFail3);
+		}
 	}
-    var srcType = Camera.PictureSourceType.CAMERA;
-    var options = camera_options(srcType);
-    var func = card_image_process;
- 
-    navigator.camera.getPicture(function cameraSuccess(imgUri) {
-          
-       if (B.card_side=='other') {       	 	
-				B.croper.destroy();
-				var options = { 
-					url:imgUri, 
-					enableOrientation:true,
-        			boundary: B.crop_opts.boundary 
-        		};
-				B.croper = new Croppie(document.getElementById('crop-box'), options);
-       	 	return false;
-       } else {
-       	func(imgUri);
-       }
-       
-    }, function cameraError(error) {
-        myApp.alert("Photo capture cancelled"); //  + error
-        card_ocr_process({vertices:[{x:0,y:0},{x:0,y:0},{x:0,y:0},{x:0,y:0}],description:''});
-        $$("#card-entry img."+B.card_side).attr("src","img/badge_none.png");
-    }, options);
-}
+	
+	function listPhoto() {
+		
+		B.dynamicPopup = myApp.popup('<div class="popup" style="overflow-y: scroll">\
+		<div class="navbar">\
+      <div class="navbar-inner">\
+        <div class="left"></div>\
+        <div class="title">List of saved card photos</div>\
+        <div class="right"><a href="#" class="link close-popup">Close</a></div>\
+      </div>\
+    </div>\
+	<div class="list">\
+		<ul id="ulPhoto"></ul>\
+	</div>\
+</div>');
 
-function card_image_process(imgUri) {
-	
-	myApp.showPreloader('Loading...');
-	setTimeout(function () {
-   	myApp.hidePreloader();
-	}, 8000);
-	card_image2dataUrl(imgUri, function(){
-      var image = new Image();
-      image.onload = function (imageEvent) {
-          var canvas = document.createElement('canvas'),
-          	  max_size = 1024,
-              width = image.width,
-              height = image.height;
-          if (width > height) {
-              if (width > max_size) {
-                  height *= max_size / width;
-                  width = max_size;
-              }
-          } 
-          else {
-              if (height > max_size) {
-                  width *= max_size / height;
-                  height = max_size;
-              }
-          }
-          canvas.width = width;
-          canvas.height = height;
-          canvas.getContext('2d').drawImage(image, 0, 0, width, height);
-          scanImg[B.card_side]['dataUrl'] = canvas.toDataURL('image/jpeg');
-          scanImg[B.card_side]['width'] = width;
-          scanImg[B.card_side]['height'] = height;
-          var pars = {
-	          photo: scanImg[B.card_side].dataUrl.replace(/^data:image\/(png|jpg|jpeg);base64,/, ''),
-	          cardid: mycard.id
-          }
-          socket.emit('card ocr', pars);
-      }
-      image.src = scanImg[B.card_side].dataUrl;
-	}); 
-}
+		if (typeof LocalFileSystem === "undefined") {
+			myApp.alert("Unable to open your file system!");
+			return false;
+		}
 
-function card_image2dataUrl(imgUri, callback) {
-	console.log('card_image2dataUrl()')
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function() {
-    var reader = new FileReader();
-    reader.onloadend = function() {
-    	scanImg[B.card_side].dataUrl = reader.result;
-      callback();
-    }
-    reader.readAsDataURL(xhr.response);
-  };
-  xhr.open('GET', imgUri);
-  xhr.responseType = 'blob';
-  xhr.send();
-}
+		window.requestFileSystem(LocalFileSystem.TEMPORARY, 0, function(fileSys) {
+			B.fs_ = fileSys;
+			B.cwd_ = B.fs_.root;
+			var html = [];
+	  		// On liste les dossiers...
+	  		ls_(function(dir_entries) {
+	         if (dir_entries.length) {
+	           console.log("dir_entries.length = "+dir_entries.length)
+	           
+	           for(var i=0; i<dir_entries.length; i++) {
+	           	 var entry = dir_entries[i];
+	           	 if (entry.isFile) {
+	           	 	dir_entries[i].remove();
+	           	 } else {
+	           	 	
+	           	 	B.dirname = entry.name;
+	           	 	
+	           	 	B.cwd_ = B.fs_.root;
+						B.cwd_.getDirectory(B.dirname, {}, function(dirEntry) {
+							B.cwd_ = dirEntry;
+							var frontfile = '', backfile = '';
+							ls_(function(file_entries) {
+	           	 			
+								if (file_entries.length) {
+									
+									for(var i=0; i<file_entries.length; i++) {
+										
+										if(file_entries[i].name=="front.png") frontfile = file_entries[i].nativeURL;
+										if(file_entries[i].name=="back.png")  backfile = file_entries[i].nativeURL;
+									}
+									
+									var dirDate = new Date(parseInt(dirEntry.name));
+									
+									$$("#ulPhoto").append('<li onClick="loadPhoto(\''+dirEntry.name+'\')" class="item-content">\
+								 <div class="item-inner item-cell">\
+								 	<div class="item-row">\
+								      <div class="item-cell">'+dirDate.toString()+'</div>\
+								    </div>\
+								    <div class="item-row">\
+								      <div class="item-cell thumb"><img src="'+ frontfile +'" /></div>\
+								      <div class="item-cell thumb"><img src="'+ backfile  +'" /></div>\
+								    </div>\
+							    </div>\
+							</li>')
+	
+								}
+	      				});
+						}, onFail);
+	           	 }
+	           }
+	         } 
+	         else {
+		         console.log("No entries...")
+	         }
+	      });
+      }, onFail);
+	}
 
-function card_ocr_process(data) {
-	console.log('card_ocr_process()');
-	// Using cropping hints from vision, we crop, rotate and show the scanned card image...
-	var points = data.vertices;
-	var x0 = points[0].x - 10;
-	var y0 = points[0].y - 10;
-	var x1 = points[2].x - x0 + 20;
-	var y1 = points[2].y - y0 + 20;
+	function capturePhoto() {
+	    if (typeof Camera === "undefined") {
+			myApp.alert("No camera available");
+	    }
+	    else {
+	    	var options = setOptions(Camera.PictureSourceType.CAMERA);
+	    	navigator.camera.getPicture( function(imageUri) {
+			    $$('#card-photo-'+B.card_side).attr("src", imageUri);
+			    
+				 B.fromfile = false;
+			    $$("#savePhoto, #processPhoto").parent().removeClass("hidden");
+			}, onFail, options);
+	    }
+	}
 	
-	var canvas = document.createElement('canvas');
-	canvas.width = x1;
-	canvas.height = y1;
-	var context = canvas.getContext('2d');
-	var cardImage = $$("#card-entry").find("img."+B.card_side);
-	cardImage.attr("src",scanImg[B.card_side].dataUrl);
+	function loadPhoto(dirname) {
+		B.cwd_ = B.fs_.root;
+		B.cwd_.getDirectory(dirname, {}, function(dirEntry) {
+			B.cwd_ = dirEntry;
+			ls_(function(file_entries) {
+				var frontfile = '', backfile = '';
+				for(var i=0; i<file_entries.length; i++) {
+					if(file_entries[i].name=="front.png") frontfile = file_entries[i].nativeURL;
+					if(file_entries[i].name=="back.png")  backfile  = file_entries[i].nativeURL;
+				}
+				$$('#card-photo-front').attr("src", frontfile);
+				$$('#card-photo-back').attr("src",  backfile);
+				$$(".button.card-side.front").trigger("click");
+				
+				//dirEntry.removeRecursively();
+			   
+			   $$("#processPhoto").parent().removeClass("hidden");
+				B.dynamicPopup.close();
+				B.fromfile = true;
+			});
+		}, onFail);
+	}
 	
-	// Using text detection result from vision, we add a formatted list of fields...
-	var ocrLines = data.description.split("\n");
+	function processPhoto() {
+		console.log('processPhoto()');
 	
-	B.container="#add_card_list";
+		myApp.showPreloader('Loading...');
+		setTimeout(function () {
+	   	myApp.hidePreloader();
+		}, 8000);
+		
+		function getDataUrl(img) {
+		   var canvas = document.createElement('canvas');
+	      canvas.width = img.width;
+	      canvas.height = img.height;
+		   var ctx = canvas.getContext('2d');
+	      ctx.drawImage(img, 0, 0); 
+	      return canvas.toDataURL('image/jpeg');
+		};
+		
+		if ($$('#card-photo-front').attr("src")) {
+			$$('#card-photo-front').on("click", function(img){
+				var dataUrl = getDataUrl(img);
+				socket.emit('card ocr', {photo:dataUrl.replace(/^data:image\/(png|jpg|jpeg);base64,/, ''), cardid: mycard.id});
+			});
+		}
+		
+		if ($$('#card-photo-back').attr("src")) {
+			$$('#card-photo-back').on("click", function(img){
+				var dataUrl = getDataUrl(img);
+				socket.emit('card ocr', {photo:dataUrl.replace(/^data:image\/(png|jpg|jpeg);base64,/, ''), cardid: mycard.id});
+			});
+		}
+	}
 	
-	if (B.card_side=='recto') {
+	socket.on('card ocr', card_ocr_process);
+	
+	function card_ocr_process(data) {
+		console.log('card_ocr_process()');
+		
+		// Using cropping hints from vision, we crop, rotate and show the scanned card image...
+		/*
+		var points = data.vertices;
+		var x0 = points[0].x - 10;
+		var y0 = points[0].y - 10;
+		var x1 = points[2].x - x0 + 20;
+		var y1 = points[2].y - y0 + 20;
+		
+		var canvas = document.createElement('canvas');
+		canvas.width = x1;
+		canvas.height = y1;
+		var context = canvas.getContext('2d');
+		var cardImage = $$("#card-entry").find("img."+B.card_side);
+		cardImage.attr("src",scanImg[B.card_side].dataUrl);
+		*/
+		
+		// Using text detection result from vision, we add a formatted list of fields...
+		var ocrLines = data.description.split("\n");
+		
+		B.container="#add_card_list";
+		B.list = "current";
+		B.index = false;
+		B.cardid = false;
+		$$(".card-fields").removeClass("hidden");
 		$$(B.container).html(base_tpl.replace(/lock/g,'unlock').replace(/{{unlock}}/g,'unlock').replace(/{{class}}/g, ''));
+		
+		/*
+		if (B.card_side=='recto') {
+			$$(B.container).html(base_tpl.replace(/lock/g,'unlock').replace(/{{unlock}}/g,'unlock').replace(/{{class}}/g, ''));
+		}
+		else { 
+			$$(".card-back-camera-open").hide(); 
+		}
+		*/
+		
+		for (var ii=0; ii<ocrLines.length; ii++) {
+			var ocrLine = ocrLines[ii].replace(/^[ ]+|[ ]+$/g,'');
+			if (ocrLine.length) add_card_li_match(ii, ocrLine);
+		}
+		
+		$$(".button-photo").addClass("hidden");
+		$$(".card-fields").parent().removeClass("hidden");
+		
+		card_init();
+		
+		myApp.hidePreloader();
 	}
-	else { 
-		$$(".card-back-camera-open").hide(); 
+
+	
+	function onFail(message) {
+	    myApp.alert('Failed because: ' + message);
 	}
 	
-	for (var ii=0; ii<ocrLines.length; ii++) {
-		var ocrLine = ocrLines[ii].replace(/^[ ]+|[ ]+$/g,'');
-		if (ocrLine.length) add_card_li_match(ii, ocrLine);
+	function onFail0(message) {
+	    myApp.alert('Failed "fileEntry.moveTo"');
+	    console.log(message)
 	}
 	
-	card_init();
-	myApp.hidePreloader();
-}
+	function onFail1(message) {
+	    myApp.alert('Failed "B.cwd_.getDirectory"');
+	    console.log(message)
+	}
+	
+	function onFail2(message) {
+	    myApp.alert('Failed "requestFileSystem"');
+	    console.log(message)
+	}
+	
+	function onFail3(message) {
+	    myApp.alert('Failed "resolveLocalFileSystemURL"');
+	    console.log(message)
+	}
+	
+	function ls_(successCallback) {
+    if (!B.fs_) {
+      return;
+    }
+    
+    var entries = [];
+    var reader = B.cwd_.createReader();
 
-socket.on('card ocr', card_ocr_process);
+    var readEntries = function() {
+      reader.readEntries(function(results) {
+        if (!results.length) {
+          entries.sort();
+          successCallback(entries);
+        } else {
+          entries = entries.concat(util.toArray(results));
+          readEntries();
+        }
+      }, onFail);
+    };
 
-var card_data = {
-	vertices:[{x:0,y:0},{x:0,y:0},{x:0,y:0},{x:0,y:0}],
-	description:'Louis Larivière \nAnalyste sénior\nCell: (514) 714-2011 \nllariviere@dubo.qc.ca \nwww.dubo.qc.ca\nDubo Électrique Ltée.\nMatériaux électriques et électroniques\n5780, rue Ontario Est \nMontréal (Québec) HIN 0A2 \nTél.:(514)255-7711 \nDirect:(514) 255-8855, poste221 \n1 800 361-4503 \nFax:(514) 255-9949'
-};
-
-var card_back_data = {
-	vertices:[{x:0,y:0},{x:0,y:0},{x:0,y:0},{x:0,y:0}],
-	description:'Verso de la carte\nAutres infos\nPlusieurs mots de plus'
-};
+    readEntries();
+  }
+  
+	var util = util || {};
+	util.toArray = function(list) {
+	  return Array.prototype.slice.call(list || [], 0);
+	};
+	
